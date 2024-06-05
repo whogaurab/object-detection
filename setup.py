6db53880080286cacba9f75c678d0ab867c82342
whogaurab <gaurabchoudhary2002@gmail.com>
2024-06-05 11:10:56 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+
+
+setup(
+    name = 'Signlanguage',
+    version = '0.0.0',
+    author = 'whogaurab',
+    author_email = 'gaurabchoudhary2002@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+
+
+
+)
